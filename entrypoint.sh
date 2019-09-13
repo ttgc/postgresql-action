cc4537c82896a43f363cdfd0c5fc08b3f76511ad
ttgc <ttgc@users.noreply.github.com>
2019-09-13 01:14:43 -0400

    Added port configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ docker_run="docker run"
 docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
 docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
-docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
+docker_run="$docker_run -d -p $INPUT_POSTGRESQL_PORT:5432 postgres:$INPUT_POSTGRESQL_VERSION"
 
 sh -c "$docker_run"
